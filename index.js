c8a91c47e84edea6f1337fe4a77e91ac390fd15e
su <mu>
2025-06-10 10:26:21 +0800

    添加一个 n 子命令(后续用于创建md文档)

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
+/** =====================================================
+ * Copyright © hk. 2022-2025. All rights reserved.
+ * File name  : 
+ * Author     : 苏木
+ * Date       : 2025-06-10
+ * Version    : 
+ * Description: 
+ * ======================================================
+ */
+const commander = require('commander');
 
-console.log(`✅ Hello, World!`);
+/** @brief 创建commander的Command实例 */
+const program = new commander.Command();
+
+/** 
+ * @brief 添加"n"子命令及其参数和动作 
+ */
+program
+	.command("n")
+	.description('print hello world!')
+	.action(() => {
+		console.log(`✅ Hello, World!`);
+	});
+
+program.parse(); // 参数处理
+
+// Try the following:
+//    node index.js n
